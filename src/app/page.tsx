9753587f15a62e5bac95d55f9ea0d52fedcecb42
roger <rogerdesouza99@gmail.com>
2024-12-17 21:59:11 -0300

    parcial do lint config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <script>
+          {'sadada'} 
+        </script>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
