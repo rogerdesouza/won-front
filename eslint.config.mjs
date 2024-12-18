0e0d9ec84fb5961b4b05862f6f83470ee493dab8
roger <roger@policiacivil.pe.gov.br>
2024-12-18 11:38:02 -0300

    ajustes no lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,12 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -14,6 +20,9 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  ...compat.config({
+    extends: ['next'],
+  }),
   {
     rules: {
       "react/react-in-jsx-scope": "off", // Desabilitar a regra que exige a importação do React
