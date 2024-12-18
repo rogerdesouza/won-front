62b2e43826007e914e1b5c6182067498a9cca1b1
roger <roger@policiacivil.pe.gov.br>
2024-12-18 15:24:51 -0300

    configuração do prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
 import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
+  baseDirectory: import.meta.dirname
 })
 
 /** @type {import('eslint').Linter.Config[]} */
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']
   },
   {
     languageOptions: { globals: globals.browser }
@@ -22,13 +22,13 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   ...compat.config({
-    extends: ["next/core-web-vitals"],
+    extends: ['next/core-web-vitals'],
     rules: {
-      "react/react-in-jsx-scope": "off", // Desabilitar a regra que exige a importação do React
-      "react/prop-types": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "@typescript-eslint/explicit-module-boundary-types": "off"
-    },
-  }),
-];
+      'react/react-in-jsx-scope': 'off', // Desabilitar a regra que exige a importação do React
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
+  })
+]
